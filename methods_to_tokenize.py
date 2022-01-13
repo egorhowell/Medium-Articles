98fc5358dcccd495b89b79dd68d61646279122b5
Egor Howell <77462366+egorhowell@users.noreply.github.com>
2022-01-13 20:58:03 +0000

    Add files via upload

---
+++ methods_to_tokenize.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+# ------- python method ---------
+string = "Hello, I'm Egor. What's your name?"
+string.split()
+
+# ------ NLTK -------------
+import nltk
+
+#download the language models
+nltk.download()
+
+# use tokenizer
+from nltk.tokenize import word_tokenize
+word_tokenize(string)
+
+# ---------- spaCy ----------
+
+# download spacy model
+get_ipython().system('python -m spacy download en_core_web_sm')
+
+# tokenize
+import spacy
+model = spacy.load("en_core_web_sm")
+doc = model(string)
+tokens =[]
+for token in doc:
+    tokens.append(token.text)
+    
+tokens
+
+# -------- gensim ------------
+
+from gensim.utils import tokenize
+list(tokenize(string))
+
+
+# In[ ]:
+
+
+
+
