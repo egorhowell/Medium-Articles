ce35cd9a39bc357bea0945082f518274ee4a33ca
Egor Howell <77462366+egorhowell@users.noreply.github.com>
2022-02-11 19:11:49 +0000

    Update markov_stationary.py

--- markov_stationary.py
+++ markov_stationary.py
@@ -1,4 +1,5 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
 # set the one-step transition
 transition_matrix =  np.array([[0, 0.5, 0.5],
@@ -13,9 +14,37 @@ for _ in range(50):
     # multiply matrix
     arr = arr @ transition_matrix
     
+print(arr)
+   
+  
+  
 # for a given starting distribution
-initial_dist = np.array([[1,0,0]])
+initial_dist = np.array([1,0,0])
 
 for _ in range(100):
-    update = initial @ transition_matrix
-    initial = update
+    update = initial_dist @ transition_matrix
+    initial_dist = update
+    
+print(initial_dist)
+  
+  
+  
+  
+# plotting the equilibrium
+def plot(steps, x, axes,title):
+    axes.plot(steps,x)
+    axes.set_title(label=title, fontdict = {'fontsize' : 18})
+    axes.set_xlabel('Steps', fontsize=14)
+    axes.set_ylabel('Probability', fontsize=14)
+    
+    for tick in axes.xaxis.get_majorticklabels().: 
+        tick.set_fontsize(14) 
+        
+    for tick in axes.yaxis.get_majorticklabels(): 
+        tick.set_fontsize(14) 
+        
+        
+fig, axes = plt.subplots(1,3,figsize=(15,5))
+plot(steps,x1,axes[0],'x1')
+plot(steps,x2,axes[1],'x2')
+plot(steps,x3,axes[2],'x3')
