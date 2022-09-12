9c0ef46de7239e6eb0e2c499b5cca28fae9d6201
Egor Howell <77462366+egorhowell@users.noreply.github.com>
2022-09-12 18:09:15 +0100

    Update markov_stationary.py

--- Statistics/markov_stationary.py
+++ Statistics/markov_stationary.py
@@ -1,6 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+# ------------------------------------------------
 # set the one-step transition
 transition_matrix =  np.array([[0, 0.4, 0.6],
                                [0.5, 0.3, 0.2],
@@ -16,8 +17,7 @@ for _ in range(50):
     
 print(arr)
    
-  
-  
+# ------------------------------------------------  
 # for a given starting distribution
 initial_dist = np.array([1,0,0])
 
@@ -27,10 +27,8 @@ for _ in range(100):
     
 print(initial_dist)
   
-  
-  
-  
-# plotting the equilibrium
+# -----------------------------------------------  
+# plotting the equilibrium for each coordinate
 def plot(steps, x, axes,title):
     axes.plot(steps,x)
     axes.set_title(label=title, fontdict = {'fontsize' : 18})
@@ -43,7 +41,22 @@ def plot(steps, x, axes,title):
     for tick in axes.yaxis.get_majorticklabels(): 
         tick.set_fontsize(14) 
         
-        
+# define the steps and empty arrays to track the coordinates 
+steps = np.arange(0,100,1)
+x1 = []
+x2 = []
+x3 = []
+
+# iterate and store the coordinates
+initial_dist = np.array([1,0,0])
+for _ in range(100):
+    x1.append(initial_dist[0])
+    x2.append(initial_dist[1])
+    x3.append(initial_dist[2])
+    update = initial_dist @ transition_matrix
+    initial_dist = update
+
+# plot the oscillation of the coordinates    
 fig, axes = plt.subplots(1,3,figsize=(15,5))
 plot(steps,x1,axes[0],'x1')
 plot(steps,x2,axes[1],'x2')
