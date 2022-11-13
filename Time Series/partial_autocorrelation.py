2d98bc32d622f70e4adf547abf13f7f15b50be61
Egor Howell <77462366+egorhowell@users.noreply.github.com>
2022-11-13 20:43:43 +0000

    Add files via upload

---
+++ Time Series/partial_autocorrelation.py
@@ -0,0 +1,33 @@
+# Import packages
+import plotly.express as px
+import pandas as pd
+import os
+from statsmodels.graphics.tsaplots import plot_pacf
+import matplotlib.pyplot as plt
+
+# Read in the data
+data = pd.read_csv('AirPassengers.csv')
+
+# Plot the data
+fig = px.line(data, x='Month', y='#Passengers',
+              labels=({'#Passengers': 'Passengers', 'Month': 'Date'}))
+
+fig.update_layout(template="simple_white", font=dict(size=18), title_text='Airline Passengers',
+                  width=650, title_x=0.5, height=400)
+
+if not os.path.exists("images"):
+    os.mkdir("images")
+
+fig.write_image("images/" + str('passenger.png'))
+fig.show()
+
+# Plot partial autocorrelation
+plt.rc("figure", figsize=(11,5))
+plot_pacf(data['#Passengers'], method='ywm')
+plt.xlabel('Lags', fontsize=18)
+plt.ylabel('Correlation', fontsize=18)
+plt.xticks(fontsize=18)
+plt.yticks(fontsize=18)
+plt.title('Partial Autocorrelation Plot', fontsize=20)
+plt.tight_layout()
+plt.show()
