e94b2135def9e3760efe7a56b7e84982516802c1
Egor Howell <egorjhowell@gmail.com>
2022-12-06 21:59:37 +0000

    simple exp smoothing script

---
+++ Time Series/simple_exp_smoothing.py
@@ -0,0 +1,38 @@
+# Import packages
+import plotly.graph_objects as go
+import pandas as pd
+import os
+from statsmodels.tsa.holtwinters import SimpleExpSmoothing
+
+# Read in the data
+data = pd.read_csv('AirPassengers.csv')
+data['Month'] = pd.to_datetime(data['Month'])
+
+# Split train and test
+train = data.iloc[:-int(len(data) * 0.2)]
+test = data.iloc[-int(len(data) * 0.2):]
+
+
+def plot_func(forecast: list[float], title: str, save_path: str) -> None:
+    """Function to plot the forecasts."""
+    fig = go.Figure()
+    fig.add_trace(go.Scatter(x=train['Month'], y=train['#Passengers'], name='Train'))
+    fig.add_trace(go.Scatter(x=test['Month'], y=test['#Passengers'], name='Train'))
+    fig.add_trace(go.Scatter(x=test['Month'], y=forecast, name='Forecast'))
+    fig.update_layout(template="simple_white", font=dict(size=18), title_text=title,
+                      width=650, title_x=0.5, height=400, xaxis_title='Date',
+                      yaxis_title='Passenger Volume')
+
+    if not os.path.exists("images"):
+        os.mkdir("images")
+
+    fig.write_image("images/" + str(save_path))
+    return fig.show()
+
+
+# Fit model and get forecasts
+model = SimpleExpSmoothing(train['#Passengers'], initialization_method='heuristic').fit(optimized=True)
+forecasts = model.forecast(len(test))
+
+# Plot the forecasts
+plot_func(forecasts, 'Simple Exponential Smoothing', 'result.png')
