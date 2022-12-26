eaac8d1e1ac4c6df76bfd0a45b456556ba7632bc
Egor Howell <egorjhowell@gmail.com>
2022-12-26 22:09:59 +0000

    adding new file

--- Time Series/Time Series Tools/cross_validation.py
+++ Time Series/Time Series Tools/cross_validation.py
@@ -1,7 +1,8 @@
 # Import packages
 import plotly.express as px
+import plotly.graph_objects as go
 import pandas as pd
-from sklearn.model_selection import TimeSeriesSplit
+from sklearn.model_selection import TimeSeriesSplit, KFold
 import os
 
 # Read in the data
@@ -14,34 +15,49 @@ df_test = data.iloc[-int(len(data) * 0.2):]
 
 
 def plot_cross_val(n_splits: int,
-                   data: pd.DataFrame,
-                   save_file_path: str) -> None:
-    """Function to plot time series cross validation splits."""
-
-    tscv = TimeSeriesSplit(n_splits=n_splits)
+                   splitter_func,
+                   df: pd.DataFrame,
+                   title_text: str,
+                   save_path: str) -> None:
     split = 1
     plot_data = []
 
-    for train_index, valid_index in tscv.split(data):
-        plot_data.append([max(train_index), 'Train', f'{split}'])
-        plot_data.append([max(valid_index)-max(train_index), 'Valid', f'{split}'])
+    for train_index, valid_index in splitter_func(n_splits=n_splits).split(df):
+        plot_data.append([train_index, 'Train', f'{split}'])
+        plot_data.append([valid_index, 'Valid', f'{split}'])
         split += 1
 
     plot_df = pd.DataFrame(plot_data, columns=['Index', 'Dataset', 'Split'])
+    plot_df = plot_df.explode('Index')
 
-    fig = px.bar(plot_df, x="Index", y="Split", color='Dataset', orientation='h',
-                 color_discrete_map={'Train': 'blue', 'Valid': 'goldenrod'})
-    fig.update_layout(template="simple_white", font=dict(size=20), title_text='Time Series Cross-Validation',
-                      title_x=0.5, width=750, height=400)
-    fig.update_traces(width=0.5)
+    fig = go.Figure()
+    for _, group in plot_df.groupby('Split'):
+        fig.add_trace(go.Scatter(x=group['Index'].loc[group['Dataset'] == 'Train'],
+                                 y=group['Split'].loc[group['Dataset'] == 'Train'],
+                                 name='Train',
+                                 line=dict(color="blue", width=10)
+                                 ))
+        fig.add_trace(go.Scatter(x=group['Index'].loc[group['Dataset'] == 'Valid'],
+                                 y=group['Split'].loc[group['Dataset'] == 'Valid'],
+                                 name='Valid',
+                                 line=dict(color="goldenrod", width=10)
+                                 ))
+
+    fig.update_layout(template="simple_white", font=dict(size=20), title_text=title_text,
+                      title_x=0.5, width=900, height=600, xaxis_title='Index', yaxis_title='Split')
+
+    names = set()
+    fig.for_each_trace(
+        lambda trace:
+        trace.update(showlegend=False)
+        if (trace.name in names) else names.add(trace.name))
 
     if not os.path.exists("../images"):
         os.mkdir("../images")
 
-    fig.write_image("../images/" + str(save_file_path))
+    fig.write_image("../images/" + str(save_path))
 
     return fig.show()
 
 
-plot_cross_val(5, df_model, 'crossval.png')
-
+plot_cross_val(n_splits=5, splitter_func=TimeSeriesSplit, df=df_model, title_text='cross-val', save_path='image.png')
