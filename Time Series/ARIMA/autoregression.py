8690cee1dbc87cff6547ac078645f774f4423ddf
Egor Howell <egorjhowell@gmail.com>
2023-01-08 20:51:19 +0000

    arima and autoreg

---
+++ Time Series/ARIMA/autoregression.py
@@ -0,0 +1,106 @@
+# Import packages
+import plotly.graph_objects as go
+import plotly.express as px
+import pandas as pd
+import matplotlib.pyplot as plt
+import os
+from statsmodels.tsa.ar_model import AutoReg, ar_select_order
+from scipy.stats import boxcox
+from scipy.special import inv_boxcox
+from statsmodels.graphics.tsaplots import plot_pacf
+from statsmodels.tsa.stattools import adfuller
+
+# Read in the data
+data = pd.read_csv('../AirPassengers.csv')
+data['Month'] = pd.to_datetime(data['Month'])
+
+
+def plot_passenger_volumes(df: pd.DataFrame,
+                           save_file_path: str) -> None:
+    """General function to plot the passenger data."""
+
+    fig = px.line(df, x='Month', y='#Passengers', labels={'Month': 'Date', '#Passengers': 'Passengers'})
+    fig.update_layout(template="simple_white", font=dict(size=18), title_text='Airline Passengers',
+                      width=650, title_x=0.5, height=400)
+
+    if not os.path.exists("../images"):
+        os.mkdir("../images")
+    fig.write_image("../images/" + str(save_file_path))
+
+    return fig.show()
+
+
+# Plot the airline passenger data
+# plot_passenger_volumes(df=data, save_file_path='passengers.png')
+
+# Make the target stationary
+data['Passengers_boxcox'], lam = boxcox(data['#Passengers'])
+data["Passenger_stationary"] = data["Passengers_boxcox"].diff()
+data.dropna(inplace=True)
+
+
+# ADF test for stationary
+def adf_test(series):
+    """Using an ADF test to determine if a series is stationary"""
+    test_results = adfuller(series)
+    print('ADF Statistic: ', test_results[0])
+    print('P-Value: ', test_results[1])
+    print('Critical Values:')
+    for threshold, adf_stat in test_results[4].items():
+        print('\t%s: %.2f' % (threshold, adf_stat))
+
+
+adf_test(data["Passenger_stationary"])
+
+# Plot partial autocorrelation
+# plt.rc("figure", figsize=(11,5))
+# plot_pacf(data['Passenger_stationary'], method='ywm')
+# plt.xlabel('Lags', fontsize=18)
+# plt.ylabel('Correlation', fontsize=18)
+# plt.xticks(fontsize=18)
+# plt.yticks(fontsize=18)
+# plt.title('Partial Autocorrelation Plot', fontsize=20)
+# plt.tight_layout()
+# plt.show()
+
+# Split train and test
+train = data.iloc[:-int(len(data) * 0.2)]
+test = data.iloc[-int(len(data) * 0.2):]
+
+# Build AR model
+selector = ar_select_order(train['Passenger_stationary'], 15)
+model = AutoReg(train['Passenger_stationary'], lags=selector.ar_lags).fit()
+
+# Get forecasts and convert to actual passenger volumes
+transformed_forecasts = list(model.forecast(steps=len(test)))
+boxcox_forecasts = []
+for idx in range(len(test)):
+    if idx == 0:
+        boxcox_forecast = transformed_forecasts[idx] + train['Passengers_boxcox'].iloc[-1]
+    else:
+        boxcox_forecast = transformed_forecasts[idx] + boxcox_forecasts[idx-1]
+
+    boxcox_forecasts.append(boxcox_forecast)
+
+forecasts = inv_boxcox(boxcox_forecasts, lam)
+
+
+def plot_forecasts(forecasts: list[float], title: str, save_path: str) -> None:
+    """Function to plot the forecasts."""
+    fig = go.Figure()
+    fig.add_trace(go.Scatter(x=train['Month'], y=train['#Passengers'], name='Train'))
+    fig.add_trace(go.Scatter(x=test['Month'], y=test['#Passengers'], name='Test'))
+    fig.add_trace(go.Scatter(x=test['Month'], y=forecasts, name='Forecast'))
+    fig.update_layout(template="simple_white", font=dict(size=18), title_text=title,
+                      width=650, title_x=0.5, height=400, xaxis_title='Date',
+                      yaxis_title='Passenger Volume')
+
+    if not os.path.exists("../images"):
+        os.mkdir("../images")
+
+    fig.write_image("../images/" + str(save_path))
+    return fig.show()
+
+
+# Plot the forecasts
+plot_forecasts(forecasts, 'Autoregression', 'result.png')
