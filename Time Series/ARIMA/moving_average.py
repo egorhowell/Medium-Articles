9b2d4ea41c506afdce08caf1b9f00da602e59d3e
Egor Howell <egorjhowell@gmail.com>
2023-01-19 21:03:49 +0000

    edit to ma model

--- Time Series/ARIMA/moving_average.py
+++ Time Series/ARIMA/moving_average.py
@@ -4,10 +4,7 @@ import plotly.express as px
 import pandas as pd
 import matplotlib.pyplot as plt
 import os
-from scipy.stats import boxcox
-from scipy.special import inv_boxcox
-from statsmodels.graphics.tsaplots import plot_pacf
-from statsmodels.tsa.stattools import adfuller
+from statsmodels.graphics.tsaplots import plot_acf
 
 # Read in the data
 data = pd.read_csv('../AirPassengers.csv')
@@ -33,36 +30,14 @@ def plot_passenger_volumes(df: pd.DataFrame,
 # Plot the airline passenger data
 plot_passenger_volumes(df=data, y='#Passengers', save_file_path='passengers_data.png')
 
-# Make the target stationary
-data['Passengers_boxcox'], lam = boxcox(data['#Passengers'])
-data["Passenger_stationary"] = data["Passengers_boxcox"].diff()
-data.dropna(inplace=True)
-
-# Plot the stationary airline passenger data
-plot_passenger_volumes(df=data, y='Passenger_stationary', save_file_path='passengers_data_stationary.png')
-
-
-# ADF test for stationary
-def adf_test(series):
-    """Using an ADF test to determine if a series is stationary"""
-    test_results = adfuller(series)
-    print('ADF Statistic: ', test_results[0])
-    print('P-Value: ', test_results[1])
-    print('Critical Values:')
-    for threshold, adf_stat in test_results[4].items():
-        print('\t%s: %.2f' % (threshold, adf_stat))
-
-
-print(adf_test(data["Passenger_stationary"]))
-
-# Plot partial autocorrelation
+# Plot autocorrelation
 plt.rc("figure", figsize=(11,5))
-plot_pacf(data['Passenger_stationary'], method='ywm')
+plot_acf(data['#Passengers'])
 plt.xlabel('Lags', fontsize=18)
 plt.ylabel('Correlation', fontsize=18)
 plt.xticks(fontsize=18)
 plt.yticks(fontsize=18)
-plt.title('Partial Autocorrelation Plot', fontsize=20)
+plt.title('Autocorrelation Plot', fontsize=20)
 plt.tight_layout()
 plt.show()
 
@@ -70,40 +45,23 @@ plt.show()
 train = data.iloc[:-int(len(data) * 0.2)]
 test = data.iloc[-int(len(data) * 0.2):]
 
-# Build AR model
-selector = ar_select_order(train['Passenger_stationary'], 15)
-model = AutoReg(train['Passenger_stationary'], lags=selector.ar_lags).fit()
 
-# Get forecasts and convert to actual passenger volumes
-transformed_forecasts = list(model.forecast(steps=len(test)))
-boxcox_forecasts = []
-for idx in range(len(test)):
-    if idx == 0:
-        boxcox_forecast = transformed_forecasts[idx] + train['Passengers_boxcox'].iloc[-1]
-    else:
-        boxcox_forecast = transformed_forecasts[idx] + boxcox_forecasts[idx-1]
-
-    boxcox_forecasts.append(boxcox_forecast)
-
-forecasts = inv_boxcox(boxcox_forecasts, lam)
-
-
-def plot_forecasts(forecasts: list[float], title: str, save_path: str) -> None:
-    """Function to plot the forecasts."""
-    fig = go.Figure()
-    fig.add_trace(go.Scatter(x=train['Month'], y=train['#Passengers'], name='Train'))
-    fig.add_trace(go.Scatter(x=test['Month'], y=test['#Passengers'], name='Test'))
-    fig.add_trace(go.Scatter(x=test['Month'], y=forecasts, name='Forecast'))
-    fig.update_layout(template="simple_white", font=dict(size=18), title_text=title,
-                      width=650, title_x=0.5, height=400, xaxis_title='Date',
-                      yaxis_title='Passenger Volume')
-
-    if not os.path.exists("../images"):
-        os.mkdir("../images")
-
-    fig.write_image("../images/" + str(save_path))
-    return fig.show()
-
-
-# Plot the forecasts
-plot_forecasts(forecasts, 'Autoregression', 'result.png')
+# def plot_forecasts(forecasts: list[float], title: str, save_path: str) -> None:
+#     """Function to plot the forecasts."""
+#     fig = go.Figure()
+#     fig.add_trace(go.Scatter(x=train['Month'], y=train['#Passengers'], name='Train'))
+#     fig.add_trace(go.Scatter(x=test['Month'], y=test['#Passengers'], name='Test'))
+#     fig.add_trace(go.Scatter(x=test['Month'], y=forecasts, name='Forecast'))
+#     fig.update_layout(template="simple_white", font=dict(size=18), title_text=title,
+#                       width=650, title_x=0.5, height=400, xaxis_title='Date',
+#                       yaxis_title='Passenger Volume')
+#
+#     if not os.path.exists("../images"):
+#         os.mkdir("../images")
+#
+#     fig.write_image("../images/" + str(save_path))
+#     return fig.show()
+#
+#
+# # Plot the forecasts
+# plot_forecasts(forecasts, 'Moving Average Model', 'result.png')
