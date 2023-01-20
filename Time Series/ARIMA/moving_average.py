72e1248bb3a4dbbe9061015ba1961569b39ef423
Egor Howell <egorjhowell@gmail.com>
2023-01-20 00:02:28 +0000

    edit to ma model

--- Time Series/ARIMA/moving_average.py
+++ Time Series/ARIMA/moving_average.py
@@ -5,6 +5,9 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import os
 from statsmodels.graphics.tsaplots import plot_acf
+from statsmodels.tsa.arima.model import ARIMA
+from scipy.stats import boxcox
+from scipy.special import inv_boxcox
 
 # Read in the data
 data = pd.read_csv('../AirPassengers.csv')
@@ -30,9 +33,17 @@ def plot_passenger_volumes(df: pd.DataFrame,
 # Plot the airline passenger data
 plot_passenger_volumes(df=data, y='#Passengers', save_file_path='passengers_data.png')
 
+# Make the target stationary
+data['Passengers_boxcox'], lam = boxcox(data['#Passengers'])
+data["Passenger_stationary"] = data["Passengers_boxcox"].diff()
+data.dropna(inplace=True)
+
+# Plot the stationary airline passenger data
+plot_passenger_volumes(df=data, y='Passenger_stationary', save_file_path='passengers_data_stationary.png')
+
 # Plot autocorrelation
 plt.rc("figure", figsize=(11,5))
-plot_acf(data['#Passengers'])
+plot_acf(data['Passenger_stationary'])
 plt.xlabel('Lags', fontsize=18)
 plt.ylabel('Correlation', fontsize=18)
 plt.xticks(fontsize=18)
@@ -41,27 +52,44 @@ plt.title('Autocorrelation Plot', fontsize=20)
 plt.tight_layout()
 plt.show()
 
+
 # Split train and test
 train = data.iloc[:-int(len(data) * 0.2)]
 test = data.iloc[-int(len(data) * 0.2):]
 
+# Train model
+model = ARIMA(train['Passenger_stationary'], order=(0, 0, 13)).fit()
 
-# def plot_forecasts(forecasts: list[float], title: str, save_path: str) -> None:
-#     """Function to plot the forecasts."""
-#     fig = go.Figure()
-#     fig.add_trace(go.Scatter(x=train['Month'], y=train['#Passengers'], name='Train'))
-#     fig.add_trace(go.Scatter(x=test['Month'], y=test['#Passengers'], name='Test'))
-#     fig.add_trace(go.Scatter(x=test['Month'], y=forecasts, name='Forecast'))
-#     fig.update_layout(template="simple_white", font=dict(size=18), title_text=title,
-#                       width=650, title_x=0.5, height=400, xaxis_title='Date',
-#                       yaxis_title='Passenger Volume')
-#
-#     if not os.path.exists("../images"):
-#         os.mkdir("../images")
-#
-#     fig.write_image("../images/" + str(save_path))
-#     return fig.show()
-#
-#
-# # Plot the forecasts
-# plot_forecasts(forecasts, 'Moving Average Model', 'result.png')
+# Get forecasts and convert to actual passenger volumes
+transformed_forecasts = list(model.forecast(steps=len(test)))
+boxcox_forecasts = []
+for idx in range(len(test)):
+    if idx == 0:
+        boxcox_forecast = transformed_forecasts[idx] + train['Passengers_boxcox'].iloc[-1]
+    else:
+        boxcox_forecast = transformed_forecasts[idx] + boxcox_forecasts[idx-1]
+
+    boxcox_forecasts.append(boxcox_forecast)
+
+forecasts = inv_boxcox(boxcox_forecasts, lam)
+
+
+def plot_forecasts(forecasts: list[float], title: str, save_path: str) -> None:
+    """Function to plot the forecasts."""
+    fig = go.Figure()
+    fig.add_trace(go.Scatter(x=train['Month'], y=train['#Passengers'], name='Train'))
+    fig.add_trace(go.Scatter(x=test['Month'], y=test['#Passengers'], name='Test'))
+    fig.add_trace(go.Scatter(x=test['Month'], y=forecasts, name='Forecast'))
+    fig.update_layout(template="simple_white", font=dict(size=18), title_text=title,
+                      width=650, title_x=0.5, height=400, xaxis_title='Date',
+                      yaxis_title='Passenger Volume')
+
+    if not os.path.exists("../images"):
+        os.mkdir("../images")
+
+    fig.write_image("../images/" + str(save_path))
+    return fig.show()
+
+
+# Plot the forecasts
+plot_forecasts(forecasts, 'Moving Average Model', 'result.png')
