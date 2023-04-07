bd821e3b24a541b7b507ef99116c6727fc9c8e28
Egor Howell <egorjhowell@gmail.com>
2023-04-07 17:55:35 +0100

    adding brute force

---
+++ Optimisation/brute-force/brute_force_search.py
@@ -0,0 +1,85 @@
+import itertools
+import math
+from typing import List, Tuple
+
+
+class BruteForceSearch:
+    """
+    A class that performs a brute force search for the optimal route between a list of cities.
+
+    Attributes:
+        cities: A list of tuples representing the cities as ((name, x, y) coordinates.
+    """
+
+    def __init__(self, cities: List[Tuple[str, float, float]]):
+        """
+        Initializes a new instance of the BruteForceSearch class.
+
+        Args:
+            cities: A list of tuples representing the cities as ((name, x, y) coordinates.
+        """
+
+        self.cities = cities
+
+    def _calculate_city_distance(self, city1: Tuple[str, float, float], city2: Tuple[str, float, float]) -> float:
+        """
+        Calculates the Euclidean distance between two cities represented as tuples (name, x, y).
+
+        Args:
+            city1: A tuple representing the coordinates of the first city (name, x, y)
+            city2: A tuple representing the coordinates of the second city (name, x, y)
+
+        Returns:
+            The Euclidean distance between the two cities.
+        """
+        _, x1, y1 = city1
+        _, x2, y2 = city2
+        return math.sqrt((x1 - x2) ** 2 + (y1 - y2) ** 2)
+
+    def _calculate_route_distance(self, route: List[Tuple[str, float, float]]) -> float:
+        """
+        Calculates the total distance of the route between a list of cities.
+
+        Args:
+            route: A list of tuples representing the cities as ((name, x, y) coordinates.
+
+        Returns:
+            The total distance of the route.
+        """
+
+        if not route:
+            raise ValueError("Cannot calculate the distance for an empty or none route")
+
+        total_distance = sum(self._calculate_city_distance(route[i], route[i + 1]) for i in range(len(route) - 1))
+        return total_distance
+
+    def run(self) -> Tuple[float, List[Tuple[str, float, float]]]:
+        """
+        Calculates the optimal route using a brute force approach.
+        Args:
+            None.
+        Returns:
+            A tuple containing the total distance of the optimal route and the corresponding route.
+        """
+        if len(self.cities) < 3:
+            raise ValueError("There must be at least 3 cities to compute the optimal route")
+
+        # Generate all permutations of the remaining cities
+        remaining_cities = self.cities[1:-1]
+        perms = itertools.permutations(remaining_cities)
+
+        # Extract the start and end cities
+        start = self.cities[0]
+        end = self.cities[-1]
+
+        # Concatenate the start and end cities to each permutation
+        permutations = [[start] + list(p) + [end] for p in perms]
+
+        # Compute all the possible solutions
+        solutions = [(self._calculate_route_distance(route), list(route)) for route in permutations]
+
+        # Get the best solution
+        best_solution = min(solutions, key=lambda x: x[0])
+        best_distance, best_route = best_solution
+
+        return best_distance, best_route
---
+++ Optimisation/brute-force/running_brute_force.py
@@ -0,0 +1,90 @@
+import plotly.graph_objects as go
+import numpy as np
+from faker import Faker
+from brute_force_search import BruteForceSearch
+import os
+
+# Generate some random city data
+n_cities = 6
+x = np.random.randint(0, 10, n_cities)
+y = np.random.randint(0, 10, n_cities)
+
+# Generate unique labels for cities using Faker
+labels = [Faker().city() for _ in range(n_cities)]
+
+# Combine data into a list of tuples
+cities = [(labels[i], x[i], y[i]) for i in range(n_cities)]
+
+# Add the first city to the end to complete the loop
+x = np.append(x, x[0])
+y = np.append(y, y[0])
+labels.append(labels[0])
+cities.append(cities[0])
+
+# Create figure
+fig = go.Figure()
+
+# Add trace for cities
+fig.add_trace(go.Scatter(x=x, y=y, mode='lines+markers+text',
+                         text=labels, textposition="top center",
+                         textfont=dict(color='black'),
+                         marker=dict(size=8, color='rgb(51, 153, 255)'),
+                         line=dict(color='rgb(51, 153, 255)', width=1),
+                         showlegend=False))
+
+# Add marker for the starting point
+fig.add_trace(go.Scatter(x=[x[0]], y=[y[0]], mode='markers',
+                         marker=dict(size=15, color='red', symbol='circle'),
+                         name='Origin City'))
+
+# Style layout
+fig.update_layout(title_text="Simulated Cities: Initial Solution",
+                  title_x=0.5, font=dict(size=18),
+                  xaxis=dict(title='X-coordinate', titlefont=dict(size=16)),
+                  yaxis=dict(title='Y-coordinate', titlefont=dict(size=16)),
+                  width=800, height=500, template='simple_white')
+
+fig.update_traces(textposition='top center', textfont=dict(size=14))
+
+if not os.path.exists("../images"):
+    os.mkdir("../images")
+fig.write_image("../images/" + str('initial_solution.png'))
+
+fig.show()
+
+# Run brute-force search
+bfs = BruteForceSearch(cities)
+best_distance, best_route = bfs.run()
+
+# Plot the results
+labels, x, y = [[value[i] for value in best_route] for i in range(3)]
+
+# Create figure
+fig = go.Figure()
+
+# Add trace for cities
+fig.add_trace(go.Scatter(x=x, y=y, mode='lines+markers+text',
+                         text=labels, textposition="top center",
+                         textfont=dict(color='black'),
+                         marker=dict(size=8, color='rgb(51, 153, 255)'),
+                         line=dict(color='rgb(51, 153, 255)', width=1),
+                         showlegend=False))
+
+# Add marker for the starting point
+fig.add_trace(go.Scatter(x=[x[0]], y=[y[0]], mode='markers',
+                         marker=dict(size=15, color='red', symbol='circle'), name='Origin City'))
+
+# Style layout
+fig.update_layout(title_text="Simulated Cities: Best Found Solution",
+                  title_x=0.5, font=dict(size=18),
+                  xaxis=dict(title='X-coordinate', titlefont=dict(size=16)),
+                  yaxis=dict(title='Y-coordinate', titlefont=dict(size=16)),
+                  width=800, height=500, template='simple_white')
+
+fig.update_traces(textposition='top center', textfont=dict(size=14))
+
+if not os.path.exists("../images"):
+    os.mkdir("../images")
+fig.write_image("../images/" + str('best_solution.png'))
+
+fig.show()
